6fd250819f6834ddaa4ecbc12dcad489940a39a1
chad <chadayop@gmail.com>
2024-02-18 18:26:49 +0800

    Set up AWS console for Active Storage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :quantity)
+    params.require(:product).permit(:name, :price, :quantity, :description, :photo)
   end
 
   def find_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_one_attached :photo
+
   validates :name, :price, :quantity, presence: true
   validates :name, format: { with: /\A[\w\s]+\z/,
         message: "should be a string or a combination
